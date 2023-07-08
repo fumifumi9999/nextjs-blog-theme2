54893b5dee69a5a3a0847caee5dbc8bb0db7e158
Fumi <fumi9999@gmail.com>
2023-07-08 21:30:00 +0900

    no loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const HomePage = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // データの非同期読み込みをシミュレートするために、一定時間後にisLoadingをfalseに設定
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
+    router.push('/index_ja.html');
   }, []);
-
-  useEffect(() => {
-    if (!isLoading) {
-      router.push('/index_ja.html');
-    }
-  }, [isLoading]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return null;
 };
